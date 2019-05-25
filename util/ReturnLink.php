99141a49e4c44c50e6dff9d94f3515651981e3f1
Vittorio Mattei <vimattei@hotmail.it>
2019-05-25 12:47:14 +0200

    Implementazione login e redirect alla pagina corrispondente

---
+++ util/ReturnLink.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace util;
+
+class ReturnLink {
+
+    public static $getReturnLink = array(
+         1 => "Home_certificatore" ,
+         2 => "Home_analista",
+         3 => "Home_ispettore",
+         4 => "Home_caricatoredb",
+         5 => "Home_gestoreprocedure",
+    ) ;
+}
